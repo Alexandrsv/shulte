97c733d6592bc1f5053ee7d4c01e5eb80bd8762f
alexandr <zbc@mail.ru>
2021-06-26 16:55:20 +0300

    Добавить таймер
    Таймер запускается при старте и останавливается в случае победы

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,38 +1,64 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import s from './Table.module.css'
 
-const SIZE = 4
+const SIZE = 3
 
 const getNewTable = (SIZE) => {
     const arrValues = Array(SIZE * SIZE).fill('').map((v, i) => i + 1)
-    return Array(SIZE).fill('').map(v => Array(SIZE).fill('').map((v) => arrValues.getRandom()))
+    return Array(SIZE).fill('').map(() => Array(SIZE).fill('').map(() => arrValues.getRandom()))
+}
+
+const increeceTime = (t) => {
+    const newTime = +t + 0.1
+    return newTime.toFixed(1)
 }
 
 const Table = () => {
     const [itemForSearch, setItemForSearch] = useState(1)
+    const [time, setTime] = useState(0)
     const [table, setNewTable] = useState([[]])
+    const [status, setStatus] = useState('waiting') //waiting|win|game
 
-    useEffect(() => {
+    const intervalRef = useRef()
+
+    useEffect(() => { //Обработка победы, остановка таймера
+        if (itemForSearch > Math.pow(table.length, 2)) {
+            setStatus('win')
+            clearInterval(intervalRef.current);
+        }
+    }, [itemForSearch, table.length])
+
+
+    useEffect(() => { //Старт
         setNewTable(getNewTable(SIZE))
-    }, [])
+    }, [setNewTable])
+
 
     const onItemClick = (e) => {
-        console.log(e.target.childNodes[0].data, Math.pow(table.length, 2))
         const item = e.target.childNodes[0].data
         if (itemForSearch === +item) {
             setItemForSearch(itemForSearch + 1)
         }
+
     }
 
     const onRestartTable = () => {
         setNewTable(getNewTable(SIZE))
         setItemForSearch(1)
+        setStatus('game')
+        clearInterval(intervalRef.current)
+        setTime(0)
+        intervalRef.current = setInterval(() => {
+            setTime(t => increeceTime(t))
+        }, 100)
     }
 
     return (
         <>
             <div className={s.Table}>
-                <h1>{itemForSearch > Math.pow(table.length, 2) ? 'Победа!' : 'Найди ' + itemForSearch}</h1>
+                <span style={{display: "inline"}}>Время:</span><span
+                style={{display: "inline", fontFamily: 'monospace'}}>{time}</span>
+                <h1>{status === 'win' ? 'Победа!' : 'Найди ' + itemForSearch}</h1>
                 {table.map((v, i) =>
                     <div key={i} className={s.Row}>
                         {
@@ -40,10 +66,14 @@ const Table = () => {
                         }
                     </div>)}
                 {itemForSearch === Math.pow(table.length + 1, 2) && <h1>Победа</h1>}
-                <button onClick={onRestartTable} className={s.Btn}>Новая таблица</button>
+                <button onClick={onRestartTable}
+                        className={s.Btn}>{status === 'win'
+                    ? 'Заново'
+                    : status === 'game' ? 'Рестарт' : 'Пуск'}
+                </button>
             </div>
         </>
-    );
-};
+    )
+}
 
 export default Table;
