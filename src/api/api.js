039ef15b4bda376ba250ac842eb325fa2c09a488
alexandr <zbc@mail.ru>
2021-06-28 01:16:32 +0300

    Добавить передачу параметров с фронта

--- src/api/api.js
+++ src/api/api.js
@@ -2,13 +2,12 @@ import axios from 'axios'
 
 export const instance = axios.create({
     baseURL: 'https://shulte.zbc.su/api/',
-    // headers: {'API-KEY': 'be82d700-54e4-4201-a22e-d509e44f0f71',}
 })
 
 
 export const getScore = (userId) => {
     return instance.get(
-        `api.php?userId=${userId}`,)
+        `api.php?userId=${userId}&${window.location.search.slice(1)}`,)
         .then(response => {
             return response.data
         })
