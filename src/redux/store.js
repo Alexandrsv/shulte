7889085c8ddfa7c27be7f4ab52158e073565ce63
alexandr <zbc@mail.ru>
2021-07-03 01:40:25 +0300

    Сохранять/загружать стейт в локальное хранилище

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@ import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import thunkMiddleware from "redux-thunk"
 import settingsReducer from "./settings-reducer";
 import scoreReducer from "./score-reducer";
+import {loadState, saveState} from "./localStorage";
+import throttle from 'lodash.throttle';
 
 let rootReducer = combineReducers({
     settingsReducer,
@@ -10,9 +12,17 @@ let rootReducer = combineReducers({
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)))
+const persistedState = loadState()
 
+const store = createStore(rootReducer, persistedState, composeEnhancers(applyMiddleware(thunkMiddleware)))
 // @ts-ignore
+store.subscribe(throttle(() => {
+    saveState({
+        ...store.getState()
+    });
+}, 1000))
+
 window.__store__ = store
 export default store
+
 
